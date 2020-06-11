0784f43a85b654dab41c2e5ec3b04b4860b3ce8e
Oktfolio <oktfolio@gmail.com>
2020-06-12 01:46:31 +0800

    nacos

--- src/main/java/com/okfolio/tero/security/filter/JsonUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/okfolio/tero/security/filter/JsonUsernamePasswordAuthenticationFilter.java
@@ -6,16 +6,16 @@ import com.okfolio.tero.security.model.UsernamePassword;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationServiceException;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.stereotype.Component;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.Assert;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,70 +26,97 @@ import java.io.InputStream;
  * @author oktfolio oktfolio@gmail.com
  * @date 2020/06/11
  */
-@Component
 public class JsonUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(JsonUsernamePasswordAuthenticationFilter.class);
 
+    // ~ Static fields/initializers
+    // =====================================================================================
+
+    private static final AntPathRequestMatcher DEFAULT_ANT_PATH_REQUEST_MATCHER =
+            new AntPathRequestMatcher("/login", "POST");
+
     private boolean postOnly = true;
 
-    public boolean isPostOnly() {
-        return postOnly;
+    private final ObjectMapper objectMapper;
+
+    // ~ Constructors
+    // ===================================================================================================
+
+    public JsonUsernamePasswordAuthenticationFilter(ObjectMapper objectMapper) {
+        super();
+        this.objectMapper = objectMapper;
     }
 
+    public JsonUsernamePasswordAuthenticationFilter(ObjectMapper objectMapper, AuthenticationManager authenticationManager) {
+        super.setAuthenticationManager(authenticationManager);
+        this.objectMapper = objectMapper;
+    }
+
+    // ~ Methods
+    // ========================================================================================================
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+        if (postOnly && !HttpMethod.POST.name().equals(request.getMethod())) {
+            throw new AuthenticationServiceException(
+                    "Authentication method not supported: " + request.getMethod());
+        }
+
+        if (request.getContentType().contains(MediaType.APPLICATION_JSON_VALUE)) {
+            logger.info("a json login request, continue");
+            UsernamePasswordAuthenticationToken authRequest =
+                    new UsernamePasswordAuthenticationToken("", "");
+            try (InputStream inputStream = request.getInputStream()) {
+                JsonParser parser = objectMapper.createParser(inputStream);
+                UsernamePassword usernamePassword = parser.readValueAs(UsernamePassword.class);
+                if (usernamePassword != null && StringUtils.isNoneEmpty(usernamePassword.getUsername())) {
+                    authRequest = new UsernamePasswordAuthenticationToken(
+                            StringUtils.trim(usernamePassword.getUsername()),
+                            usernamePassword.getPassword());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                logger.error("failed to obtain json from request, {}", e.getMessage());
+                authRequest = new UsernamePasswordAuthenticationToken(
+                        "", "");
+            }
+            setDetails(request, authRequest);
+            return this.getAuthenticationManager().authenticate(authRequest);
+        } else {
+            // if not JSON request，continue with attemptAuthentication
+            logger.info("not a json request, use default UsernamePasswordAuthenticationFilter");
+            return super.attemptAuthentication(request, response);
+        }
+    }
+
+    /**
+     * Provided so that subclasses may configure what is put into the authentication
+     * request's details property.
+     *
+     * @param request that an authentication request is being created for
+     * @param authRequest the authentication request object that should have its details
+     * set
+     */
+    @Override
+    protected void setDetails(HttpServletRequest request,
+                              UsernamePasswordAuthenticationToken authRequest) {
+        authRequest.setDetails(authenticationDetailsSource.buildDetails(request));
+    }
+
+    /**
+     * Defines whether only HTTP POST requests will be allowed by this filter. If set to
+     * true, and an authentication request is received which is not a POST request, an
+     * exception will be raised immediately and authentication will not be attempted. The
+     * <tt>unsuccessfulAuthentication()</tt> method will be called as if handling a failed
+     * authentication.
+     * <p>
+     * Defaults to <tt>true</tt> but may be overridden by subclasses.
+     */
     @Override
     public void setPostOnly(boolean postOnly) {
         this.postOnly = postOnly;
     }
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request,
-                                                HttpServletResponse response)
-            throws AuthenticationException {
-
-        logger.info("JsonUsernamePasswordAuthenticationFilter attemptAuthentication");
-        if (postOnly && !HttpMethod.POST.name().equals(request.getMethod())) {
-            throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
-        } else {
-            if (request.getContentType().contains(MediaType.APPLICATION_JSON_VALUE)) {
-                logger.info("a json login request, continue");
-                UsernamePasswordAuthenticationToken authenticationToken;
-                try (InputStream inputStream = request.getInputStream()) {
-                    JsonParser parser = objectMapper.createParser(inputStream);
-                    UsernamePassword usernamePassword = parser.readValueAs(UsernamePassword.class);
-
-                    //TODO
-                    if (usernamePassword == null) {
-                        throw new BadCredentialsException("");
-                    }
-                    if (StringUtils.isBlank(usernamePassword.getUsername())) {
-                        throw new BadCredentialsException("");
-                    }
-
-                    if (StringUtils.isBlank(usernamePassword.getPassword())) {
-                        throw new BadCredentialsException("");
-                    }
-
-                    logger.info("json login, user: {}", usernamePassword.getUsername());
-                    authenticationToken = new UsernamePasswordAuthenticationToken(
-                            usernamePassword.getUsername(), usernamePassword.getPassword());
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    logger.error("failed to obtain json from request, {}", e.getMessage());
-                    authenticationToken = new UsernamePasswordAuthenticationToken(
-                            "", "");
-                }
-                setDetails(request, authenticationToken);
-                return this.getAuthenticationManager().authenticate(authenticationToken);
-            } else {
-                // if not JSON request，continue with attemptAuthentication
-                logger.info("not a json request, use default UsernamePasswordAuthenticationFilter");
-                return super.attemptAuthentication(request, response);
-            }
-        }
-    }
 }
