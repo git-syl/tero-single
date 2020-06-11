d1d2c7ba3b5503f9c3b61069f8b051d39f04d9e2
Oktfolio <oktfolio@gmail.com>
2020-06-12 02:23:25 +0800

    nacos

--- src/main/java/com/okfolio/tero/security/userdetails/TeroUserDetails.java
+++ src/main/java/com/okfolio/tero/security/userdetails/TeroUserDetails.java
@@ -2,7 +2,6 @@ package com.okfolio.tero.security.userdetails;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.Set;
