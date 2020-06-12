f59bf18fc664e70f8b735f2c1a08b958bafde16d
Oktfolio <oktfolio@gmail.com>
2020-06-12 22:13:06 +0800

    nacos

---
+++ src/main/java/com/okfolio/tero/modules/admin/controller/AdminDepartmentController.java
@@ -0,0 +1,8 @@
+package com.okfolio.tero.modules.admin.controller;
+
+/**
+ * @author oktfolio oktfolio@gmail.com
+ * @date 2020/06/12
+ */
+public class AdminDepartmentController {
+}
--- src/main/java/com/okfolio/tero/security/model/PhoneCode.java
+++ src/main/java/com/okfolio/tero/security/model/PhoneCode.java
@@ -5,4 +5,39 @@ package com.okfolio.tero.security.model;
  * @date 2020/06/12
  */
 public class PhoneCode {
+    /**
+     * phone number
+     */
+    private String phone;
+    /**
+     * authentication code
+     */
+    private String code;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public PhoneCode() {
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneCode{" +
+                "phone='" + phone + '\'' +
+                ", code='" + code + '\'' +
+                '}';
+    }
 }
