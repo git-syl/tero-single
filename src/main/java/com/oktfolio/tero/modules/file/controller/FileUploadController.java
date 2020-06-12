16a42b7efe10d0193dd69f39a0a817227ed460f7
Oktfolio <oktfolio@gmail.com>
2020-06-12 23:33:38 +0800

    nacos

--- src/main/java/com/oktfolio/tero/modules/file/controller/FileUploadController.java
+++ src/main/java/com/oktfolio/tero/modules/file/controller/FileUploadController.java
@@ -1,8 +1,13 @@
 package com.oktfolio.tero.modules.file.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author oktfolio oktfolio@gmail.com
  * @date 2020/06/12
  */
+@RestController
+@RequestMapping("")
 public class FileUploadController {
 }
