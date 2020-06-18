3272da929c6060582499569073fd9c81ca61b642
Oktfolio <oktfolio@gmail.com>
2020-06-18 11:54:04 +0800

    Update LogoutSuccessHandlerImpl.java

--- src/main/java/com/oktfolio/tero/security/handlers/LogoutSuccessHandlerImpl.java
+++ src/main/java/com/oktfolio/tero/security/handlers/LogoutSuccessHandlerImpl.java
@@ -4,6 +4,7 @@ import com.oktfolio.tero.utils.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
